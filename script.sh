236246237177d6c21dbfeeebfb0087b1ea4b3e20
pwnish3r <leonardeuw@gmail.com>
2024-12-10 00:51:30 +0200

    Commit

--- script.sh
+++ script.sh
@@ -64,12 +64,13 @@ mount /dev/${partition1} /mnt/efi
 echo "Part 1 done"
 read answer
 
-
-
-: '
 # 6. Install the base system and essential packages
 echo "Installing the base system..."
 pacstrap -K /mnt base base-devel linux-lts linux-lts-headers linux-firmware git btrfs-progs grub efibootmgr grub-btrfs inotify-tools timeshift intel-ucode nano networkmanager networkmanager-iwd pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber reflector zsh openssh man-db man-pages texinfo sudo
+
+echo "Done"
+read answer
+
 
 # 7. Generate the fstab file
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -77,43 +78,25 @@ cat /mnt/etc/fstab
 
 # 8. Chroot into the new system
 arch-chroot /mnt <<EOF
-# Set time zone and hardware clock
 ln -sf /usr/share/zoneinfo/Europe/Bucharest /etc/localtime
 hwclock --systohc
-
-# Generate locale
 vim /etc/locale.gen  # Pause so user can modify locale.gen
-
 locale-gen
-
-# Set up locales
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "KEYMAP=en" >> /etc/vconsole.conf
 echo "ArchLinux" >> /etc/hostname
 echo "127.0.0.1 localhost\n::1 localhost\n127.0.1.1 Arch" >> /etc/hosts
-
-# Set root password
 passwd
-
-# Ask for a username
 echo "Enter a username for the new user:"
 read username
 useradd -mG wheel $username
 passwd $username
-
-# Enable sudo for the new user
 echo "Uncommenting the wheel group in sudoers..."
 sed -i '/# %wheel ALL=(ALL) ALL/s/^# //' /etc/sudoers
-
-# Install and configure GRUB
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB  
 grub-mkconfig -o /boot/grub/grub.cfg
-
-# Enable NetworkManager
 systemctl enable NetworkManager
 EOF
-
 # 9. Unmount the partitions and reboot
 echo "Unmounting the system and rebooting..."
 umount -R /mnt
-'
