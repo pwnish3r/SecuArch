3b2f5ab958d5fee55e9c12a8244f94d1d28db17a
pwnish3r <leonardeuw@gmail.com>
2024-12-24 18:38:59 +0200

    update

--- postInstall/after_install_2.sh
+++ postInstall/after_install_2.sh
@@ -3,34 +3,35 @@ echo "Now you can choose to install either i3 or bspwm. For any other WM or DE, 
 echo "Your choice: "
 read choice
 if [ "$choice" == "i3" ]; then
-	yay -S i3 rofi thunar xclip clipmenu dunst feh picom xss-lock lxappearance arandr
-	git clone https://github.com/pwnish3r/dotfiles-i3.git
-	cd dotfiles-i3
-	cp -r .config $HOME/
-	echo "Install script 2/2 complete. Do you want to reboot now? (yes/no)"
-	read reboot_now
-	if [ "$reboot_now" == "yes" ]; then
-    		umount -R /mnt
-    		reboot
-	else
-    		echo "You can reboot later with the 'reboot' command."
+    yay -S i3 rofi thunar xclip clipmenu dunst feh picom xss-lock lxappearance arandr
+    git clone https://github.com/pwnish3r/dotfiles-i3.git
+    cd dotfiles-i3
+    cp -r .config $HOME/
+    echo "Install script 2/2 complete. Do you want to reboot now? (yes/no)"
+    read reboot_now
+    if [ "$reboot_now" == "yes" ]; then
+        umount -R /mnt
+        reboot
+    else
+        echo "You can reboot later with the 'reboot' command."
+    fi
+elif [ "$choice" == "bspwm" ]; then
+    curl -L https://is.gd/gh0stzk_dotfiles -o $HOME/RiceInstaller
+    chmod +x RiceInstaller
+    ./RiceInstaller
+    cd $HOME/.config
+    git clone https://github.com/pwnish3r/dotfiles-bspwm.git
+    cd dotfiles-bspwm
+    cp -r bspwm ../
+    cp -r nvim ../
+    cp -r tmux ../
+    cp -r zsh ../
+    echo "Install script 2/2 complete. Do you want to reboot now? (yes/no)"
+    read reboot_now
+    if [ "$reboot_now" == "yes" ]; then
+        reboot
+    else
+        echo "You can reboot later with the 'reboot' command."
+    fi
 fi
-else if ["$choice" == "bspwm" ]; then
-	curl -L https://is.gd/gh0stzk_dotfiles -o $HOME/RiceInstaller
-	chmod +x RiceInstaller
-	./RiceInstaller
-	cd $HOME/.config
-	git clone https://github.com/pwnish3r/dotfiles-bspwm.git
-	cd dotfiles-bspwm
-	cp -r bspwm ../
-	cp -r nvim ../
-	cp -r tmux ../
-	cp -r zsh ../
-	echo "Install script 2/2 complete. Do you want to reboot now? (yes/no)"
-	read reboot_now
-	if [ "$reboot_now" == "yes" ]; then
-    		reboot
-	else
-    		echo "You can reboot later with the 'reboot' command."
-fi	
-fi
+
