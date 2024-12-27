e31210acbbe643b1818f983e379b5594d6d357b8
pwnish3r <leonardeuw@gmail.com>
2024-12-27 02:17:01 +0200

    Update

--- chroot_script.sh
+++ chroot_script.sh
@@ -35,8 +35,8 @@ echo "Making post install scripts executable..."
 chmod +x SecuArch/postInstall/after_install_*.sh
 chmod +x SecuArch/*.sh
 echo "Activating post install scripts autorun..."
-sed -i "s|/home/user/|/home/$username/|g" SecuArch/script-scheduler.service
-sed -i "s|^User=user$|User=$username|g"
+sed -i "s|/home/user|/home/$username|g" SecuArch/script-scheduler.service
+sed -i "s|User=user|User=$username|g"
 # sed -i "s|^SCRIPT_DIR.*postInstall\"$|SCRIPT_DIR=\"/home/$username/auxiliary_scripts/SecuArch/postInstall\"|g" SecuArch/scriptScheduler.sh
 sudo mv SecuArch/script-scheduler.service /etc/systemd/system/
 sudo systemctl daemon-reload
