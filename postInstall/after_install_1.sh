8b767ded5a710a18b1642d8f1078176faa8bb13e
pwnish3r <leonardeuw@gmail.com>
2024-12-29 00:59:06 +0200

    Update

--- postInstall/after_install_1.sh
+++ postInstall/after_install_1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+######################################################################################
 installPackages(){
 	package_file="$HOME/auxiliary_scripts/SecuArch/postInstall/packages.txt"
 	mapfile -t packages < "$package_file"
@@ -11,11 +12,10 @@ installPackages(){
 	done
 	echo "Installation process complete."
 }
+######################################################################################
 
-
-
-sudo cp -r $HOME/auxiliary_scripts/SecuArch/grubTheme/CyberEXS /boot/grub/themes
-sudo sed -i 's|^#GRUB_THEME=.*|GRUB_THEME=/boot/grub/themes/CyberEXS/theme.txt|' /etc/default/grub
+sudo cp -r $HOME/auxiliary_scripts/SecuArch/grubTheme/dedsec /boot/grub/themes
+sudo sed -i 's|^#GRUB_THEME=.*|GRUB_THEME=/boot/grub/themes/dedsec/theme.txt|' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 cd $HOME/auxiliary_scripts
 curl -O https://blackarch.org/strap.sh
@@ -24,8 +24,7 @@ sudo ./strap.sh
 sudo sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/grub-btrfsd --syslog --timeshift-auto|' /usr/lib/systemd/system/grub-btrfsd.service
 sudo systemctl enable grub-btrfsd
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-
-sudo systemctl enable sddm
+installPackages
 systemctl enable vmtoolsd.service
 systemctl start vmtoolsd.service
 systemctl enable ufw
