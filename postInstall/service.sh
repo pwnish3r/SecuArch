e7a0cfc3edd1d7556eaaa9a6e5a9635082b3c5b4
pwnish3r <leonardeuw@gmail.com>
2025-04-01 10:28:21 +0300

    wallpapers

--- postInstall/service.sh
+++ postInstall/service.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-mv ~/.zshrc-dot ~/.zshrc
 sed -i \"s|^$HOME/auxiliary_scripts.*$||g\" ~/.bashrc
+rm -rf ~/Pictures/wallpapers
+mkdir ~/Pictures/wallpapers
+cp ~/auxiliary_scripts/SecuArch/media/SecuArchBlack.jpg
+cp ~/auxiliary_scripts/SecuArch/media/SecuArchRed.jpg
 rm -f ~/.config/systemd/user/service.service
 rm -f ~/auxiliary_scripts/SecuArch/postInstall/service.sh
 rm -rf ~/auxiliary_scripts/SecuArch
